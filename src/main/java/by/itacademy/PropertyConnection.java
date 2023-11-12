a633e4042e785f73f97131e56df2573a3cbf82da
Kseniya Kozlova <123176964+KseniyaKozlova@users.noreply.github.com>
2023-11-12 16:44:12 +0300

    Properties file name rewrite as constance

--- src/main/java/by/itacademy/PropertyConnection.java
+++ src/main/java/by/itacademy/PropertyConnection.java
@@ -7,6 +7,7 @@ import java.util.Properties;
 public class PropertyConnection {
 
     private static final String WEATHER_URI = "service.weather.uri";
+    private static final String PROPERTIES_NAME = "config.properties";
 
     public String getProperty() {
         try (InputStream inputStream = PropertyConnection.class.getClassLoader().getResourceAsStream("config.properties")) {
