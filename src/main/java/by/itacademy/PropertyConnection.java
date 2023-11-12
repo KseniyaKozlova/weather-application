3f2c2de234f3927f670ddb1f3b39c1f6efdb8f22
Kseniya Kozlova <123176964+KseniyaKozlova@users.noreply.github.com>
2023-11-12 17:46:22 +0300

    Deleted properties file name constant

--- src/main/java/by/itacademy/PropertyConnection.java
+++ src/main/java/by/itacademy/PropertyConnection.java
@@ -7,10 +7,9 @@ import java.util.Properties;
 public class PropertyConnection {
 
     private static final String WEATHER_URI = "service.weather.uri";
-    private static final String PROPERTIES_NAME = "config.properties";
-
+    
     public String getProperty() {
-        try (InputStream inputStream = PropertyConnection.class.getClassLoader().getResourceAsStream(PROPERTIES_NAME)) {
+        try (InputStream inputStream = PropertyConnection.class.getClassLoader().getResourceAsStream("config.properties")) {
 
             final Properties properties = new Properties();
             properties.load(inputStream);
